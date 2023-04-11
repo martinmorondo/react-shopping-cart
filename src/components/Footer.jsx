705c54a9cf89c2b60439810bddfae4e54416e488
Martín Morondo <martinmorondo@gmail.com>
2023-04-11 09:35:55 -0300

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import './footer.css';
+
+export function Footer () {
+    return (
+        <footer className='footer'>
+            <h5>Shopping Cart with UseContext and useReducer</h5>
+        </footer>
+    )
+}
